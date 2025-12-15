ee93218614221b5251e23ba6267650eb3b8dc504
tanmaydhelia <tanmaydhelia03@gmail.com>
2025-12-15 16:55:21 +0530

    Added Search Flight Comp, Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
+import { SearchFlights } from './components/search-flights/search-flights';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'search', component: SearchFlights }
+];
--- src/app/components/search-flights/search-flights.ts
+++ src/app/components/search-flights/search-flights.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FlightService } from '../../services/flight';
+import { FlightSummary } from '../../models/flight-app';
 
 @Component({
   selector: 'app-search-flights',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './search-flights.html',
   styleUrl: './search-flights.css',
 })
 export class SearchFlights {
+  private fb = inject(FormBuilder);
+  private flightService = inject(FlightService);
 
+  searchResults = signal<FlightSummary[]>([]);
+
+  searchForm = this.fb.group({
+    from: ['JFK', Validators.required],
+    to: ['LHR', Validators.required],
+    journeyDate: ['', Validators.required],
+    tripType: ['ONE_WAY', Validators.required]
+  });
+
+  onSearch() {
+    if (this.searchForm.valid) {
+      const criteria = this.searchForm.value as any;
+      
+      this.flightService.searchFlights(criteria).subscribe(data => {
+        // Update the signal with the new data
+        this.searchResults.set(data);
+      });
+    }
+  }
 }
